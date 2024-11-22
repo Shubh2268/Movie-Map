1176c19774488ed3dd8b7cb31861f682116b2358
Shubh <shubh2268@gmail.com>
2024-11-22 17:43:19 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Hi World! This is Shubham Tiwari :@</h1>
+      <h1 className='text-3xl font-bold underline'>Hi World! This is Shubham Tiwari!</h1>
     </>
   )
 }
