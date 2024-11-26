a250eff21cfcf7094e0afb97c7c39de6e4ccb5f3
Shubh <shubh2268@gmail.com>
2024-11-26 19:57:18 +0530

    --

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-center'>Hi World! This is Shubham Tiwari!</h1>
+      <Navbar />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <header className='fixed top-0 w-full h-16 bg-black bg-opacity-70 z-40'>
+            <div className='container mx-auto px-3 flex items-center h-full'>
+                <div>
+                    <h1 className='text-4xl items-center font-bold px-4 bg-gradient-to-r from-[#02AABD] to-[#00CDAC] inline-block text-transparent bg-clip-text'>moviemap</h1>
+                </div>
+
+                <nav className='hidden lg:flex items-center gap-1 ml-5 text-white'>
+                    <a href="">Movies</a>
+                    <a href="">TV Shows</a>
+                </nav>
+            </div>
+        </header>
+    )
 }
 
 export default Navbar
