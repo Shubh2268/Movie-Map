125fc2f289c5d91e7c3b9074959d956c0fb1470f
Shubh <shubh2268@gmail.com>
2024-11-27 20:45:20 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { IoSearchOutline } from "react-icons/io5";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <header className='fixed top-0 w-full h-16 bg-black bg-opacity-70 z-40'>
             <div className='container mx-auto px-3 flex items-center h-full'>
                 <div>
-                    <h1 className='text-3xl rounded-full items-center font-bold px-4 bg-gradient-to-b from-[#D4145A] to-[#FBB03B] inline-block text-transparent bg-clip-text uppercase underline'>moviemap</h1>
+                    <h1 className='text-3xl items-center font-bold px-4 bg-gradient-to-b from-[#ff8c00] to-[#ff0080] inline-block text-transparent bg-clip-text font-logoFont cursor-pointer active:scale-95 transition-all'>moviemap</h1>
                 </div>
 
                 <nav className='hidden lg:flex items-center gap-1 ml-5 text-white'>
@@ -20,10 +21,13 @@ const Navbar = () => {
                             placeholder='Search here...'
                             className='bg-transparent px-4 py-1 outline-none border-none hidden lg:block placeholder:text-gray-300 text-white'
                         />
-                        <button className='text-2xl text-white'>
+                        <button className='text-2xl text-white active:scale-90 transition-all'>
                             <IoSearchOutline />
                         </button>
                     </form>
+                    <div className='text-3xl text-red-400 cursor-pointer active:scale-90 transition-all'>
+                        <FaUserCircle />
+                    </div>
                 </div>
             </div>
         </header>
