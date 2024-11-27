123086fd6f1606d934673e7984551964f3fce9b8
Shubh <shubh2268@gmail.com>
2024-11-27 17:48:25 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,17 @@ const Navbar = () => {
         <header className='fixed top-0 w-full h-16 bg-black bg-opacity-70 z-40'>
             <div className='container mx-auto px-3 flex items-center h-full'>
                 <div>
-                    <h1 className='text-4xl items-center font-bold px-4 bg-gradient-to-r from-[#02AABD] to-[#00CDAC] inline-block text-transparent bg-clip-text'>moviemap</h1>
+                    <h1 className='text-3xl rounded-full items-center font-bold px-4 bg-gradient-to-b from-[#D4145A] to-[#FBB03B] inline-block text-transparent bg-clip-text uppercase underline'>moviemap</h1>
                 </div>
 
                 <nav className='hidden lg:flex items-center gap-1 ml-5 text-white'>
-                    <a href="">Movies</a>
-                    <a href="">TV Shows</a>
+                    <a className='px-2' href="">Movies</a>
+                    <a className='px-2' href="">TV Shows</a>
                 </nav>
+
+                <div>
+
+                </div>
             </div>
         </header>
     )
