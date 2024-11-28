992e5fa636e7b994ccfe6ddc12ab3db54c07e99b
Shubh <shubh2268@gmail.com>
2024-11-28 17:51:33 +0530

    --

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { IoSearchOutline } from "react-icons/io5";
-import { FaUserCircle } from "react-icons/fa";
+import { MdOutlineLightMode } from "react-icons/md";;
 
 const Navbar = () => {
     return (
         <header className='fixed top-0 w-full h-16 bg-black bg-opacity-70 z-40'>
-            <div className='container mx-auto px-3 flex items-center h-full'>
+            <div className='container mx-auto px-5 flex items-center h-full'>
                 <div>
-                    <h1 className='text-3xl items-center font-bold px-4 bg-gradient-to-b from-[#ff8c00] to-[#ff0080] inline-block text-transparent bg-clip-text font-logoFont cursor-pointer active:scale-95 transition-all'>moviemap</h1>
+                    <h1 className='text-2xl items-center font-extrabold px-2 bg-gradient-to-b from-[#2193b0] to-[#6dd5ed] inline-block text-transparent bg-clip-text font-logoFont cursor-pointer active:scale-95 transition-all uppercase'>moviemap</h1>
                 </div>
 
                 <nav className='hidden lg:flex items-center gap-1 ml-5 text-white'>
@@ -19,14 +19,14 @@ const Navbar = () => {
                     <form className='flex items-center gap-2'>
                         <input type='text'
                             placeholder='Search here...'
-                            className='bg-transparent px-4 py-1 outline-none border-none hidden lg:block placeholder:text-gray-300 text-white'
+                            className='bg-transparent pr-4 pl-2 py-1 outline-none border-b border-gray-500 hidden lg:block placeholder:text-gray-300 text-white'
                         />
-                        <button className='text-2xl text-white active:scale-90 transition-all'>
+                        <button className='text-xl text-white active:scale-90 transition-all'>
                             <IoSearchOutline />
                         </button>
                     </form>
-                    <div className='text-3xl text-red-400 cursor-pointer active:scale-90 transition-all'>
-                        <FaUserCircle />
+                    <div className='text-xl text-cyan-400 cursor-pointer active:scale-90 transition-all'>
+                        <MdOutlineLightMode />
                     </div>
                 </div>
             </div>
