8fbe1d93b52e890e2c4e25bb7f83e0d8d4831bbb
Shubh <shubh2268@gmail.com>
2024-12-02 17:24:45 +0530

    --

--- src/components/ShiftButton.jsx
+++ src/components/ShiftButton.jsx
@@ -6,10 +6,12 @@ const ShiftButton = () => {
     const tabs = ['Popular', 'Upcoming', 'Top Rated'];
 
     return (
-        <div className='flex justify-around items-center space-x-2 bg-transparent my-20 p-1 rounded-full border border-green-400'>
-            {tabs.map((tab) => (
-                <button key={tab} onClick={() => setActiveTab(tab)} className={`px-14 py-2 rounded-full font-medium transition-all ${activeTab === tab ? 'bg-gradient-to-r from-teal-500 to-green-500 text-white' : 'bg-transparent text-green-400 hover:text-white hover:bg-green-400'}`}>{tab}</button>
-            ))}
+        <div className='flex justify-center items-center'>
+            <div className='flex justify-around items-center space-x-2 bg-transparent my-20 p-1 rounded-full border border-green-400'>
+                {tabs.map((tab) => (
+                    <button key={tab} onClick={() => setActiveTab(tab)} className={`px-14 py-1 rounded-full font-medium text-sm transition-all ${activeTab === tab ? 'bg-gradient-to-r from-teal-500 to-green-500 text-white' : 'bg-transparent text-green-400 hover:text-white hover:bg-green-400'}`}>{tab}</button>
+                ))}
+            </div>
         </div>
     );
 };
