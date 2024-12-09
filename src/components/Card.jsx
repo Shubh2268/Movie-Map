3049395f9f2c40ef3d96f210ec6faef84d10c511
Shubh <shubh2268@gmail.com>
2024-12-09 17:49:01 +0530

    --

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,14 +9,14 @@ const Card = ({ movie }) => {
   return (
     <div className='group bg-gray-800 rounded-lg shadow-md shadow-teal-600 overflow-hidden relative'>
       <div className='relative h-[500px]'>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='w-full h-full object-cover transition-transform transform group-hover:scale-105 duration-500 ease-in-out' />
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className='w-full h-full object-cover transition-transform transform group-hover:scale-105 duration-700 ease-in-out' />
       </div>
 
       <div className='absolute inset-0 bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 transition-opacity flex flex-col justify-end p-4'>
         <h3 className='text-white font-bold text-lg truncate'>{movie.title}</h3>
         <p className='text-gray-100 text-sm py-1'>{movie.release_date}</p>
-        <p className='text-gray-200 text-sm'>{movie.overview}</p>
-        <p className='text-teal-300 font-semibold'>
+        <p className='text-gray-200 text-sm'>{movie.overview.slice(0, 200) + '...'}</p>
+        <p className='text-teal-400 font-semibold'>
           Rating: {movie.vote_average.toFixed(1)} / 10
         </p>
       </div>
