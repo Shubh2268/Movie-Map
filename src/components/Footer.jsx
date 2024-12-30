2291f227999f3f2f377405782c0a178a81241119
Shubh <shubh2268@gmail.com>
2024-12-30 20:15:56 +0530

    --

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
     return (
-        <footer className='bg-black mt-8 py-8 border-t border-teal-700'>
+        <footer className='bg-gray-100 dark:bg-black mt-8 py-8 border-t border-teal-700'>
             <div className='container mx-auto px-6 lg:px-16 text-center'>
                 {/* Logo and Tagline */}
                 <div className='mb-6'>
                     <Link to='/' className='text-xl lg:text-2xl font-extrabold px-2 bg-gradient-to-r from-teal-400 to-green-400 inline-block text-transparent bg-clip-text font-logoFont cursor-pointer active:scale-95 transition-all tracking-wide'>moviemap</Link>
 
-                    <p className='text-sm mt-2 text-gray-400'>Discover the best movies and TV shows, all in one place.</p>
+                    <p className='text-sm mt-2 text-gray-800 dark:text-gray-400'>Discover the best movies and TV shows, all in one place.</p>
                 </div>
 
                 {/* Social Media Links */}
@@ -31,8 +31,8 @@ const Footer = () => {
 
                 {/* Footer Text */}
                 <div className='text-sm text-gray-500'>
-                    <p className='text-gray-400'> &copy; {new Date().getFullYear()} <span className='text-teal-400 font-medium'>MovieMap</span>. All rights reserved </p>
-                    <p className='mt-2 text-gray-400'> Designed and Developed with ❤️ by the <span className='uppercase'>Shubham Tiwari.</span></p>
+                    <p className='text-gray-800 dark:text-gray-400'> &copy; {new Date().getFullYear()} <span className='text-teal-400 font-medium'>MovieMap</span>. All rights reserved </p>
+                    <p className='mt-2 text-gray-800 dark:text-gray-400'> Designed and Developed with ❤️ by the <span className='uppercase'>Shubham Tiwari.</span></p>
                 </div>
             </div>
         </footer>
